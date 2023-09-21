5ab7077f5d3b8474eecb9503d5f55880fb000074
Hyper-Dragon <61525854+Hyper-Dragon@users.noreply.github.com>
2023-09-21 18:16:24 +0100

    fix #26

--- ChessDotComSharp/Client.Base.cs
+++ ChessDotComSharp/Client.Base.cs
@@ -15,6 +15,7 @@ namespace ChessDotComSharp
         public ChessDotComClient()
         {
             _client = new HttpClient();
+            _client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36");
         }
 
         private async Task<T> GetAsync<T>(string uri)
